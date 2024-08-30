f69c9917a6087006c8b8aaa4f929fc24651052bb
RomuloDsouza <0000402106@senaimgaluno.com.br>
2024-08-29 21:06:00 -0300

    2

--- minha-api/server.js
+++ minha-api/server.js
@@ -5,13 +5,42 @@ const porta = 80
 api.get('/', (req, res) => {
     const rotaPadrao = {
         nome_rota: '/',
-        codigo_status: '200',
+        nome: 'Romulo',
+        idade: 30,
         metodo: 'GET'
     }
 
     res.status(200)
     res.json(rotaPadrao)
 })
+
+// api.post('/cliente/novo', (req, res) => {
+//     const response = {
+//         mensagem:"Deu certo",
+//         status:201
+//     }
+
+//     res.status(201)
+//     res.json(response)
+// })
+
+
+// Cria usuarios
+api.post('/clientes/novo', (req, res) => {
+
+    const response = [
+        {
+            mensagem: 'Cliente criado com sucesso',
+            status: 201
+        }
+    ]
+
+    res.status(201)
+    res.json(response)
+})
+
+
+
 
 api.listen(porta, () => {
     console.log(`Servidor em execução na porta ${porta}`)
