b18c525ec2090a8a4d4a6355f3d3ec8e0087d738
RomuloDsouza <0000402106@senaimgaluno.com.br>
2024-09-02 21:03:53 -0300

    2

--- minha-api/server.js
+++ minha-api/server.js
@@ -1,69 +1,37 @@
-const express = require('express')
-const api = express()
-const porta = 80
 
-api.get('/', (req, res) => {
+const express = require('express');
+const api = express();
+const porta = 80;
+
+// Chave de API (simulada)
+const API_KEY = '1234567890abcdef';
+
+// Middleware de autenticação por chave de API na URL
+function authenticateApiKey(req, res, next) {
+    const apiKey = req.params.apiKey;
+    if (apiKey && apiKey === API_KEY) {
+        next(); // Chave válida, prossiga para o próximo middleware ou rota
+    } else {
+        res.status(401).json({ mensagem: 'acesso nao autorizado', cod_status: 401 });
+    }
+}
+
+// Endpoint com autenticação usando a chave na URL
+api.get('/:apiKey', authenticateApiKey, (req, res) => {
     const rotaPadrao = {
         nome_rota: '/',
         nome: 'Romulo',
         idade: 30,
         metodo: 'GET'
-    }
-
-    res.status(200)
-    res.json(rotaPadrao)
-})
-
-// api.post('/cliente/novo', (req, res) => {
-//     const response = {
-//         mensagem:"Deu certo",
-//         status:201
-//     }
-
-//     res.status(201)
-//     res.json(response)
-// })
-
-
-// Cria usuarios
-api.post('/clientes/novo', (req, res) => {
-
-    const response = [
-        {
-            mensagem: 'Cliente criado com sucesso',
-            status: 201
-        }
-    ]
-
-    res.status(201)
-    res.json(response)
-})
-
-
-api.put("/cliente/update/cpfcnpj/12345678901", (req, res) => {
-    const response = {
-      mesagem: "Dados atualizados com sucesso",
-      status: 200,
     };
-    res.status(200);
-    res.json(response);
-  });
 
-  api.delete('/cliente/delete/cpfcnpj/12345678901' , (req, res) => {
-    const response = {
-      mesagem: "Dados atualizados com sucesso",
-      status: 201,
-    };
-    res.status(201);
-    res.json(response);
-  });
-   
+    res.status(200).json({ mensagem: 'acesso autorizado', cod_status: 200, rotaPadrao });
+});
 
-
-
-
+// Iniciando o servidor
 api.listen(porta, () => {
-    console.log(`Servidor em execução na porta ${porta}`)
-})
+    console.log(`Servidor em execução na porta ${porta}`);
+});
+
 
 
